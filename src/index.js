17e57ea088450ee652f7778327386a5d8cf6c538
kanu-e <chukwuemekakanu@gmail.com>
2021-04-24 22:45:39 -0500

    display cards

--- src/index.js
+++ src/index.js
@@ -1,14 +1,51 @@
-const cardDivs = document.getElementById("cards-div")
+const cardsDiv = document.getElementById("cards-div")
+const URL = "http://localhost:3000"
+
+const cardsURL = `${URL}/cards`
+
 
 class Card {
 
     static allCards = []
 
     constructor(card){
-        this.name = card.name;
-        this.number = card.number;
+        this.number = card.card_number;
         this.id = card.id;
-        card.allCards.push(this)
+        this.transactions = card.transactions
+        this.user = card.user
+        this.employer = card.employer
+        Card.allCards.push(this)
+    }   
+
+    static renderCards(){
+        for(let card of this.allCards){
+            card.renderCard();
+        }
     }
-    
+
+    renderCard(){     
+        let p = document.createElement('a') 
+        let button = document.createElement('button')
+        let cardDetailsDiv = document.createElement('div')
+        let cardDiv = document.createElement('div')
+        button.innerHTML = "view details" 
+        console.log(this)
+        p.innerHTML = this.number      
+        cardDiv.append(p, button)
+        cardsDiv.append(cardDiv)
+    }
+
+    static fetchCards(){
+        fetch(cardsURL)
+        .then (rsp => rsp.json())
+        .then(cards =>{
+            for(let card of cards){
+                 new Card(card)
+            }
+            this.renderCards()
+        })
+    }
+
 }
+
+Card.fetchCards()
