6fb0e286cd626adb9410f9349f3c7284336f82dd
kanu-e <chukwuemekakanu@gmail.com>
2021-04-24 19:41:20 -0500

    create card class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('booo')
+const cardDivs = document.getElementById("cards-div")
+
+class Card {
+
+    static allCards = []
+
+    constructor(card){
+        this.name = card.name;
+        this.number = card.number;
+        this.id = card.id;
+        card.allCards.push(this)
+    }
+    
+}
